ea8204d65277643426805ecd45114f4653b28d22
Hubert Gad <50676544+hubertgad@users.noreply.github.com>
2020-09-23 22:55:32 +0200

    Comments removed

--- Startup.cs
+++ Startup.cs
@@ -16,8 +16,6 @@ namespace Portfolio
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
@@ -26,7 +24,6 @@ namespace Portfolio
             services.AddTransient<IEmailService, EmailService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,7 +34,6 @@ namespace Portfolio
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
